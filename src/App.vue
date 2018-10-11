b842d5537858e6e2c0e89a0ab082fda6530c66bd
mukeke <mukeke@quandashi.com>
2018-10-11 17:30:22 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
+    <layout></layout>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Layout from './components/layout/Layout.vue'
+
 export default {
+  components: {Layout},
   name: 'App'
 }
 </script>
---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+  data () {
+    return {
+      msg: 'layout'
+    }
+  },
+  created () {
+    console.log(this.$route)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
