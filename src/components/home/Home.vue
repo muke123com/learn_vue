77f3a308935a5c5cf9ce18e9dd3fac3a9f8f5d20
mukeke <mukeke@quandashi.com>
2018-11-01 18:25:28 +0800

    vue

---
+++ src/components/home/Home.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <h1 class="title">{{ msg }}</h1>
+        <div class="css-test">css测试</div>
+        <ul>
+            <li v-for="item in niceMap">{{item['num']}} {{item['name']}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        data() {
+            return {
+                msg: '123',
+                niceMap: []
+            }
+        },
+        created() {
+            this.getData();
+            console.log(this.common);
+        },
+        methods: {
+            getData: function () {
+                this.niceMap = niceMap;
+                console.log(niceMap);
+            }
+        }
+    }
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    @import "home.css";
+</style>
