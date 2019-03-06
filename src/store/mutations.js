4748abbc438cbbb8962115dd22515a0af272eb87
mukeke <mukeke@quandashi.com>
2019-03-06 18:43:17 +0800

    vuex

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -9,20 +9,35 @@ export default {
     //改变算式
     ['CLICK_CALC_BTN'](state, key){
         if(state['re']){
-            state.calc.formula = '';
             state['re'] = false;
         }
-
+        if(['←','CE','C','±','√','1/x'].indexOf(key) != -1){
+            state['re'] = true;
+        }
         switch (key){
             case '←':
-
+                state.calc.formula = state.calc.formula.substr(0,state.calc.formula.length-1);
+                break;
+            case 'CE':
+                state.calc.formula = "";
+                state.calc.result = 0;
+                break;
+            case 'C':
+                state.calc.formula = "";
+                break;
+            case '±':
+                state.calc.formula = -state.calc.formula;
+                break;
+            case '√':
+                state.calc.formula = -state.calc.formula;
                 break;
         }
-
         state.calc.key = key
     },
     ['CHANGE_FORMULA'](state){
-        state.calc.formula += state.calc.key
+        if(!state['re']){
+            state.calc.formula += state.calc.key.toString();
+        }
     },
     ['GET_RESULT'](state){
         state['re'] = true;
