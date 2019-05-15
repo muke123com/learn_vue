69f17352f25d9d72bace58a323e82257bfbcd1fa
mukeke <mukeke@quandashi.com>
2019-05-15 20:05:42 +0800

    h5

--- src/components/Learn/h5/index.vue
+++ src/components/Learn/h5/index.vue
@@ -1,21 +1,25 @@
 <template>
-    <div class="h5-box">
-        <canvas-box></canvas-box>
+    <div>
+        <h5api></h5api>
     </div>
 </template>
 <script>
-    import CanvasBox from './canvas'
+    import H5api from './H5API'
+    import { mapState,mapMutations,mapActions,mapGetters } from 'vuex'
     export default {
         components: {
-            CanvasBox
+            H5api
         },
         data(){
             return {
 
             }
         },
+        created() {
+            this.CHANGE_TITLE({title: 'H5'})
+        },
         methods: {
-
+            ...mapMutations(['CHANGE_TITLE']),
         }
     }
 </script>
